e70e17be877e0e40f2edbb1e531ad732ec0b0330
Marc Johlic <marc.johlic@gmail.com>
2015-07-27 13:37:02 -0400

    Initial commit

---
+++ Lesson 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson 1
+//
+//  Created by Marc Johlic on 7/27/15.
+//  Copyright (c) 2015 pogul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# AppleProg-Git-Tutorials
